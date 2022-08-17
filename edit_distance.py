05212224942b3326a3561f0c697601e46c22d455
Saber <48609695+naythan-c@users.noreply.github.com>
2022-08-17 15:10:02 -0700

    Completed bonus challenge, using transpositions

--- edit_distance.py
+++ edit_distance.py
@@ -30,3 +30,39 @@ def edit_distance(word1, word2):
 
     # Return the final value in our matrix, which corresponds with the edit distance
     return matrix[len(word1)][len(word2)]
+
+def edit_distance_transpositions(word1, word2):
+    #Check if there's an empty string, if there is: raise an error
+    if not word1 or not word2:
+        raise ValueError("Empty strings are not allowed!")
+    # Create our matrix
+    matrix = [[0] * (len(word2) + 1) for i in range(len(word1) + 1)]
+
+    # Fill the top row 
+    for i in range(len(word2) + 1):
+        matrix[0][i] = i
+
+    # Fill the first column 
+    for j in range(len(word1) + 1):
+        matrix[j][0] = j
+
+    # Iterate bottom up through our matrix, utilizing dynamic programming to fill our matrix until we figure out the edit distance
+    for index1, character1 in enumerate(word1, 1):
+        for index2, character2 in enumerate(word2, 1):
+            if character1 == character2:  # if the last characters match, it's the same edit distance as without the last character
+                # set matrix value to the value without the last character
+                matrix[index1][index2] = matrix[index1 - 1][index2 - 1]
+            #Transposition Case
+            elif (index1 > 1 and index2 > 1) and word1[index1 - 1] == word2[index2 - 2] and word1[index1 - 2] == word2[index2 - 1]: #check if switching the adjacent characters will make the substring match
+                matrix[index1][index2] = matrix[index1 - 2][index2 - 2] + 1 #set your value to the edit distance of the substring without the two characters we just swapped
+            else:
+                # if the last characters don't match, find the minimum edit distance between the insert, delete, and replace edits
+                matrix[index1][index2] = min(
+                    matrix[index1][index2 - 1], matrix[index1 - 1][index2], matrix[index1 - 1][index2 - 1], ) + 1
+
+    # If you want to visualize the matrix, uncomment this line of code:
+    # print('\n'.join(['\t'.join([str(cell) for cell in row])
+    #       for row in matrix]))
+
+    # Return the final value in our matrix, which corresponds with the edit distance
+    return matrix[len(word1)][len(word2)]
--- test_edit_distance.py
+++ test_edit_distance.py
@@ -3,6 +3,7 @@ import edit_distance
 
 class TestEditDistance(unittest.TestCase):
 
+    #Unit Tests for Edit Distance with Only Insertions, Deletions, and Substitutions
     def test_edit_distance(self):
         #Valid Cases 
         self.assertEqual(edit_distance.edit_distance("snowy", "sunny"), 3) #tests valid words
@@ -18,6 +19,21 @@ class TestEditDistance(unittest.TestCase):
         self.assertRaises(ValueError, edit_distance.edit_distance, "exponetial", "") #makes sures an empty string for word2 raises an error
         self.assertRaises(ValueError, edit_distance.edit_distance, "", "") #makes sures an empty string for both word1 and word2 raises an error
 
+    #Unit Tests for Edit Distance with Transpositions 
+    def test_edit_distance_transpositions(self):
+        #Valid Cases 
+        self.assertEqual(edit_distance.edit_distance_transpositions("snowy", "sonyw"), 2) #tests valid words
+        self.assertEqual(edit_distance.edit_distance_transpositions("polynomial", "opylnomial"), 2) #tests valid words
 
+        #Boundary Cases
+        self.assertEqual(edit_distance.edit_distance_transpositions("a", "aaaaaaaaaaa"), 10) #tests if there's only one character for word1
+        self.assertEqual(edit_distance.edit_distance_transpositions("a", "a"), 0) #tests if both words are the same
+        self.assertEqual(edit_distance.edit_distance_transpositions("1", "123"), 2) #tests strings with numbers
+        self.assertEqual(edit_distance.edit_distance_transpositions("exponential", "polynomial"), 6) #tests strings when no transpositions are required
+
+        #Erroneous Cases
+        self.assertRaises(ValueError, edit_distance.edit_distance_transpositions, "", "exponetial") #makes sures an empty string for word1 raises an error
+        self.assertRaises(ValueError, edit_distance.edit_distance_transpositions, "exponetial", "") #makes sures an empty string for word2 raises an error
+        self.assertRaises(ValueError, edit_distance.edit_distance_transpositions, "", "") #makes sures an empty string for both word1 and word2 raises an error
 if __name__ == '__main__':
     unittest.main()
